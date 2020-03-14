d0bc624a306fe3878bdf19cfd988c72cff0dc2bf
hayeon song <shy2907@hanmail.net>
2020-03-14 21:14:20 +0900

    Update 20200314

---
+++ src/main/java/com/hayeon/springboot/web/PostsApiController.java
@@ -0,0 +1,33 @@
+package com.hayeon.springboot.web;
+
+
+import com.hayeon.springboot.domain.posts.PostsRepository;
+import com.hayeon.springboot.service.PostsSerivce;
+import com.hayeon.springboot.web.dto.PostsResponseDto;
+import com.hayeon.springboot.web.dto.PostsSaveRequestDto;
+import com.hayeon.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsSerivce postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PostMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    @GetMapping("api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
+}
